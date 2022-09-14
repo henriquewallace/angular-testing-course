07ccb96b44aa03f89c404309765b9bafd1f6d26c
henriquewallace <henrique.wallace@townsq.com.br>
2022-09-13 22:36:48 -0300

    TST - Test setup using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,21 +2,26 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
+  let loggerSpy: any;
+  let calculator: CalculatorService;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('Logger Service', ["log"]);
+    calculator = new CalculatorService(loggerSpy);
+  });
 
   it('should add two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
-
     const result = calculator.add(2, 2);
 
     expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
-
     const result = calculator.subtract(4, 2);
 
     expect(result).toBe(2);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
 });
