0c0a5a6a65c2c9545c1e27dded13d5fa7f4b8af0
henriquewallace <henrique.wallace@townsq.com.br>
2022-09-18 18:08:54 -0300

    TST - Test error handling

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,4 +1,5 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { Course } from './../model/course';
@@ -70,6 +71,21 @@ describe("CoursesService", () => {
       ...changes
     })
   });
+
+  it('should give an error if save changes fails', () => {
+    const changes: Partial<Course> = { titles: { description: 'Testing Course' } };
+
+    coursesService.saveCourse(12, changes)
+      .subscribe(() => fail('the save course operation should have failed'),
+      (error: HttpErrorResponse) => {
+        expect(error.status).toBe(500);
+      })
+
+      const req = httpTestingController.expectOne('/api/courses/12');
+
+      expect(req.request.method).toEqual('PUT');
+      req.flush('Save course failed', { status: 500, statusText: 'Internal Server Error' });
+    });
 
   afterEach(() => {
     httpTestingController.verify();
