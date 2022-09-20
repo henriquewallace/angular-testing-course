5c5b3d9a5cff0fcdb601070aee4c27bff9b00382
henriquewallace <henrique.wallace@townsq.com.br>
2022-09-20 14:27:50 -0300

    TST - Test should display the first course

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -43,9 +43,17 @@ describe('CoursesCardListComponent', () => {
 
 
   it("should display the first course", () => {
+    component.courses = setupCourses();
+    fixture.detectChanges();
 
-      pending();
+    const course = component.courses[0];
+    const card = el.query(By.css('.course-card:first-child'));
+    const title = card.query(By.css('mat-card-title'));
+    const image = card.query(By.css('img'));
 
+    expect(course).toBeTruthy();
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
 
 
